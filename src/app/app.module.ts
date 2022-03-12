1a17906a442365a5af538d279b37cb7452ad29bb
announa <moupou@posteo.org>
2022-03-12 16:43:06 -0500

    created board, column and navigation component, basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { BoardColumnComponent } from './board-column/board-column.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    BoardColumnComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
