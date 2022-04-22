491854e0bde8834e8a47a7671864a6e5032c20b3
announa <moupou@posteo.org>
2022-04-21 19:10:03 -0500

    navigation: responsive

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirestoreService } from '../services/firestore.service';
 
@@ -8,11 +8,19 @@ import { FirestoreService } from '../services/firestore.service';
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit, AfterViewInit {
+  navIsOpen = false;
+  navIconClose = false;
+  @HostListener('window:resize', ['$event'])
+  resize() {
+    this.resetNav();
+  }
+
   constructor(
     public fireService: FirestoreService,
     public router: Router,
     private cd: ChangeDetectorRef
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {}
 
@@ -29,4 +37,17 @@ export class NavigationComponent implements OnInit, AfterViewInit {
   goToLogin() {
     this.router.navigateByUrl('/');
   }
+
+  toggleMenu() {
+    this.navIsOpen = !this.navIsOpen;
+    setTimeout(() => {
+      this.navIconClose = ! this.navIconClose
+    }, 200);
+  }
+
+  resetNav() {
+    if (window.innerWidth >= 700) {
+      this.navIsOpen = false;
+      this.navIconClose = false;
+  }}
 }
